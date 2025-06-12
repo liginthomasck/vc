522196aee387e97a2996e74bfc346f622c6e7006
Ligin Thomas CK <66181008+liginthomasck@users.noreply.github.com>
2025-06-12 11:22:34 +0530

    update

--- js/main.js
+++ js/main.js
@@ -18,10 +18,10 @@
 
     // Sticky Navbar
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 45) {
-            $('.nav-bar').addClass('sticky-top');
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
         } else {
-            $('.nav-bar').removeClass('sticky-top');
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
         }
     });
     
@@ -40,42 +40,45 @@
     });
 
 
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Date and time picker
+    $('.date').datetimepicker({
+        format: 'L'
+    });
+    $('.time').datetimepicker({
+        format: 'LT'
+    });
+
+
     // Header carousel
     $(".header-carousel").owlCarousel({
         autoplay: true,
         smartSpeed: 1500,
-        items: 1,
-        dots: true,
         loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ]
+        nav: false,
+        dots: true,
+        items: 1,
+        dotsData: true,
     });
 
 
     // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
+    $('.testimonial-carousel').owlCarousel({
         autoplay: true,
         smartSpeed: 1000,
-        margin: 24,
-        dots: false,
         loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            992:{
-                items:2
-            }
-        }
+        nav: false,
+        dots: true,
+        items: 1,
+        dotsData: true,
     });
+
     
 })(jQuery);
 
